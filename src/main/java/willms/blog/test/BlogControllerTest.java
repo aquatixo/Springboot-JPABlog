c5a21282aceb3c22028a6917801f6faa25a2f202
aquatixo <hyunms@gmail.com>
2021-01-12 15:29:57 +0900

    settings done

---
+++ src/main/java/willms/blog/test/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package willms.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+	//http://localhost:8002/test/hello
+	@GetMapping("test/hello")
+	public String hello() {
+		return "<h1>hello Spring boot</h1>";
+	}
+}
